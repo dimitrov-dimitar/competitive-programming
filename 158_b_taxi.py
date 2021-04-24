6f591b1e03ec99d74215402254d6ab75ff9f59b1
dimitar <dimitar.kr.dimitrov@gmail.com>
2021-04-24 23:36:08 +0300

    added new problems

--- 158_b_taxi.py
+++ 158_b_taxi.py
@@ -5,17 +5,44 @@ s = [int(x) for x in input().split()]
 
 sum_s = sum(s)
 taxi = 0
-# 3
-# 3 3 2
-# 3
 
 s = sorted(s, reverse=True)
-
-# for group in s:
-#     if group == 4:
-#         taxi += 1
 
 four = s.count(4)
 three = s.count(3)
 two = s.count(2)
 one = s.count(1)
+
+# four
+taxi += four
+
+# two
+if two == 1:
+    taxi += 1
+elif two % 2 == 0:
+    taxi += two / 2
+else:
+    taxi += (two / 2) + 1
+
+# three and one
+reminder = one - three
+if three > one:
+    taxi += three
+elif three == one:
+    taxi += three
+elif three < one:
+    taxi += three
+    if two % 2 == 0:
+        if reminder % 4 == 0:
+            taxi += (reminder / 4)
+        else:
+            taxi += (reminder // 4) + 1
+    else:
+        if reminder > 2:
+            reminder -= 2
+            if reminder % 4 == 0:
+                taxi += (reminder / 4)
+            else:
+                taxi += (reminder // 4) + 1
+
+print(int(taxi))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,4 +3,46 @@
 n = int(input())
 s = [int(x) for x in input().split()]
 
-print((sum(s) // 4) + 1)
+sum_s = sum(s)
+taxi = 0
+
+s = sorted(s, reverse=True)
+
+four = s.count(4)
+three = s.count(3)
+two = s.count(2)
+one = s.count(1)
+
+# four
+taxi += four
+
+# two
+if two == 1:
+    taxi += 1
+elif two % 2 == 0:
+    taxi += two / 2
+else:
+    taxi += (taxi / 2) + 1
+
+# three and one
+reminder = one - three
+if three > one:
+    taxi += three
+elif three == one:
+    taxi += three
+elif three < one:
+    taxi += three
+    if two % 2 == 0:
+        if reminder % 4 == 0:
+            taxi += (reminder / 4)
+        else:
+            taxi += (reminder // 4) + 1
+    else:
+        if reminder > 2:
+            reminder -= 2
+            if reminder % 4 == 0:
+                taxi += (reminder / 4)
+            else:
+                taxi += (reminder // 4) + 1
+
+print(int(taxi))
