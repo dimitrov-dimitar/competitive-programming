27d06d1b55135ef0212c0bc63eed1c63133d86e8
dimitar <dimitar.kr.dimitrov@gmail.com>
2021-08-31 14:07:50 +0300

    added new problems

---
+++ CodeForces/easy problems/1399_a_remove_smallest.py
@@ -0,0 +1,17 @@
+# https://codeforces.com/problemset/problem/1399/A
+
+t = int(input())
+
+for _ in range(t):
+    length_a = int(input())
+    list_a = [int(x) for x in input().split()]
+    list_a.sort()
+    while True:
+        if len(list_a) == 1:
+            print('YES')
+            break
+        elif abs(list_a[0] - list_a[1]) <= 1:
+            list_a.pop(0)
+        else:
+            print('NO')
+            break
---
+++ CodeForces/easy problems/581_a_vasya_the_hipster.py
@@ -0,0 +1,15 @@
+# https://codeforces.com/problemset/problem/581/A
+
+a, b = [int(x) for x in input().split()]
+
+min_num = min(a, b)
+max_num = max(a, b)
+
+result = max_num - min_num
+
+if result < 2:
+    result = 0
+else:
+    result /= 2
+
+print(min_num, int(result), end=' ')
