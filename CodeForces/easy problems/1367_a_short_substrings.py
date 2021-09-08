40ce7d9fd368d62c5cf4de1fd7d8040e06b5ad64
dimitar <dimitar.kr.dimitrov@gmail.com>
2021-09-08 16:16:06 +0300

    added new problems

---
+++ CodeForces/easy problems/1367_a_short_substrings.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/1367/A
+
+t = int(input())
+
+list_a = []
+
+for _ in range(t):
+    b = input()
+    if len(b) == 2:
+        print(b)
+        continue
+    else:
+        for n in range(0, len(b), 2):
+            list_a.append(b[n])
+        list_a.append(b[-1])
+    print(''.join(list_a))
+    list_a = []
+
