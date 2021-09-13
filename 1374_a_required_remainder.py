f9b8fa32807cc13e7cda436a91626d6ee8d7fae8
dimitar <dimitar.kr.dimitrov@gmail.com>
2021-09-14 00:29:22 +0300

    added new problems

---
+++ 1374_a_required_remainder.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/problemset/problem/1374/A
+
+t = int(input())
+
+for _ in range(t):
+    x, y, n = [int(x) for x in input().split()]
+
+    if (n - n % x + y <= n):
+        print(n - n % x + y)
+    else:
+        print(n - n % x - (x - y))
