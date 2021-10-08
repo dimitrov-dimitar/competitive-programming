fffa9a55584e01787b6d4262356dc9d9a4ff3989
dimitar <dimitar.kr.dimitrov@gmail.com>
2021-10-08 20:25:53 +0300

    added new problems

--- 313_a_ilya_and_bank_account.py
+++ 313_a_ilya_and_bank_account.py
@@ -4,8 +4,14 @@ n = int(input())
 
 if n >= 0:
     print(n)
-elif n > -12:
-    print(0)
 else:
-    last = abs(n) % 10
-    print(last)
+    # last = abs(n) % 10
+    last = str(n)[-1]
+    prev = str(n)[-2]
+
+    new_num = str(n)
+    if last >= prev:
+        new_num = new_num[:-1]
+    else:
+        new_num = new_num[:-2] + last
+    print(int(new_num))
