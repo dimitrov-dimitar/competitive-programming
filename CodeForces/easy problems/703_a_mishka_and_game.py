7e71b4980dc1136079adcc0975e9b3f08b1a54c5
dimitar <dimitar.kr.dimitrov@gmail.com>
2021-10-08 23:04:25 +0300

    added new problems

---
+++ CodeForces/easy problems/703_a_mishka_and_game.py
@@ -0,0 +1,23 @@
+# https://codeforces.com/problemset/problem/703/A
+
+n = int(input())
+
+m_counter = 0
+c_counter = 0
+
+for _ in range(n):
+    x, y = [int(x) for x in input().split()]
+    if x == y:
+        m_counter += 1
+        c_counter += 1
+    elif x > y:
+        m_counter += 1
+    else:
+        c_counter += 1
+
+if m_counter > c_counter:
+    print('Mishka')
+elif m_counter < c_counter:
+    print('Chris')
+else:
+    print('Friendship is magic!^^')
