8ad2574479de0e6f75a5a8c38c55e492767f4922
dimitar <dimitar.kr.dimitrov@gmail.com>
2021-10-11 13:50:42 +0300

    added new problems

---
+++ 1433_a_boring_apartments.py
@@ -0,0 +1,13 @@
+# https://codeforces.com/problemset/problem/1433/A
+
+t = int(input())
+
+l_num = [1, 2, 3, 4]
+counter = 0
+
+for _ in range(t):
+    x = input()
+    first_digit = int(x[0])
+    counter = (first_digit - 1) * sum(l_num) + sum(l_num[:len(x)])
+    print(counter)
+    counter = 0
