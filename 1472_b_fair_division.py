d4aa8460f3ac70e0bf753c3f7bbd1b503da7874b
dimitar <dimitar.kr.dimitrov@gmail.com>
2022-02-09 18:46:15 +0200

    added new problems

---
+++ 1472_b_fair_division.py
@@ -0,0 +1,40 @@
+# https://codeforces.com/problemset/problem/1472/B
+
+t = int(input())
+
+for _ in range(t):
+    list_1 = []
+    list_2 = []
+    yes_var = ""
+    break_var = False
+
+    n = int(input())
+    num_arr = [int(x) for x in input().split()]
+    for num in num_arr:
+        if num == 1:
+            list_1.append(num)
+        else:
+            list_2.append(num)
+
+    sum_list = sum(list_1) + sum(list_2)
+
+    if sum_list % 2 == 1:
+        print("NO")
+    else:
+        target = sum_list / 2
+
+        for _ in range(len(list_1) + len(list_2)):
+            if break_var:
+                break
+            for a in range(len(list_1) + 1):
+                if break_var:
+                    break
+                for b in range(len(list_2) + 1):
+                    if a + 2 * b == target:
+                        yes_var = True
+                        break_var = True
+                        break
+        if yes_var:
+            print("YES")
+        else:
+            print("NO")
--- 489_c_given_length_and_sum_of_digits.py
+++ 489_c_given_length_and_sum_of_digits.py
@@ -1,3 +1,34 @@
 # https://codeforces.com/problemset/problem/489/C
 
-m, s = [ioctl]
+m, s = [int(x) for x in input().split()]
+
+min_num = int("1" + ("0" * (m - 1)))
+max_num = int("9" * m)
+
+# first_num = -1
+# last_num = -1
+
+
+def sum_digits(n):
+    s = 0
+    while n:
+        s += n % 10
+        n //= 10
+    return s
+
+
+if s > max_num or s < min_num:
+    print(-1, -1)
+else:
+
+    for x in range(min_num, max_num + 1):
+        if sum_digits(x) == s:
+            first_num = x
+            break
+
+    for x in range(max_num, min_num - 1, -1):
+        if sum_digits(x) == s:
+            last_num = x
+            break
+
+    print(first_num, last_num)
