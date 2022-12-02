f40e4a6c4eecb714a617e17b0a639d451c2c82b9
dimitar <dimitar.kr.dimitrov@gmail.com>
2022-12-02 22:22:17 +0200

    AoC 2022

--- Advent of Code/2022/1/1.py
+++ Advent of Code/2022/1/1.py
@@ -25,6 +25,17 @@ for row in matrix:
         calories = 0
 
 # print(elves)
-max_num = max(elves, key=elves.get)
-print(max_num)
-print(elves[max_num])
+print(max(elves.values()))
+
+# Part Two
+
+new_dict = dict(sorted(elves.items(), key=lambda item: item[1]))
+# print(new_dict)
+
+top_three_elves = (
+    list(new_dict.values())[-1]
+    + list(new_dict.values())[-2]
+    + list(new_dict.values())[-3]
+)
+
+print(top_three_elves)
